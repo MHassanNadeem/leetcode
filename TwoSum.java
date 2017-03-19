d55a7751888d3c44a0319bb8eef9dfb7fbd0fede
Hassan Nadeem <m.hassan.nadeem@outlook.com>
2017-03-19 17:20:59 +0500

    Add #1

--- TwoSum.java
+++ TwoSum.java
@@ -6,28 +6,28 @@ import java.util.HashMap;
 /* #1 Two Sum
  * href: https://leetcode.com/problems/two-sum/ */
 public class TwoSum {
-	
-	public static int[] solve(int[] list, int target){		
-		int dual;
-		/* Key = a number from the list, Value = Index of that number */
-		HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
-		
-		for(int i=0; i<list.length; i++){
-			dual = target - list[i];
-			if(map.containsKey(dual)){
-				return new int[]{map.get(dual), i};
-			}
-			
-			map.put(list[i], i);
-		}
-		
-		throw new IllegalArgumentException("No Solution for provided arguments");
-	}
+    
+    public static int[] solve(int[] list, int target){        
+        int dual;
+        /* Key = a number from the list, Value = Index of that number */
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        
+        for(int i=0; i<list.length; i++){
+            dual = target - list[i];
+            if(map.containsKey(dual)){
+                return new int[]{map.get(dual), i};
+            }
+            
+            map.put(list[i], i);
+        }
+        
+        throw new IllegalArgumentException("No Solution for provided arguments");
+    }
 
-	public static void main(String[] args) {
-		int[] list = new int[]{3, 2, 4};
-		int target = 6;
-		
-		System.out.println(Arrays.toString( solve(list, target) ));
-	}
+    public static void main(String[] args) {
+        int[] list = new int[]{3, 2, 4};
+        int target = 6;
+        
+        System.out.println(Arrays.toString( solve(list, target) ));
+    }
 }
