734c98ac34376f4d2480f489f75ef1dafb436042
Hassan Nadeem <m.hassan.nadeem@outlook.com>
2017-03-24 21:48:21 +0500

    Add log(n) solution

--- SearchInsertPosition.java
+++ SearchInsertPosition.java
@@ -10,6 +10,25 @@ import java.util.Arrays;
 public class SearchInsertPosition {
     
     public static int solve(int[] nums, int target){
+        int lo = 0;
+        int hi = nums.length-1;
+        int mid;
+        
+        while(lo <= hi){
+            mid = (hi+lo)/2;
+            if(target == nums[mid]){
+                return mid;
+            }else if(target > nums[mid]){
+                lo = mid + 1;
+            }else{
+                hi = mid - 1;
+            }
+        }
+        
+        return lo;
+    }
+    
+    public static int solveLinear(int[] nums, int target){
         int i;
         for(i=0; i<nums.length && nums[i]<target; i++);
         return i;
@@ -22,5 +41,8 @@ public class SearchInsertPosition {
         assertEquals(1, solve(new int[]{1,3,5,6}, 2));
         assertEquals(4, solve(new int[]{1,3,5,6}, 7));
         assertEquals(0, solve(new int[]{1,3,5,6}, 0));
+        assertEquals(0, solve(new int[]{1,3,5}, 0));
+        assertEquals(1, solve(new int[]{1,3,5}, 3));
+        assertEquals(3, solve(new int[]{1,3,5}, 7));
     }
 }
