646666be91bdcbe61a29c9cb3390872386efaa71
Hassan Nadeem <m.hassan.nadeem@outlook.com>
2017-03-26 21:15:48 +0500

    Move ListNode anon class to new file

--- AddTwoNumbers.java
+++ AddTwoNumbers.java
@@ -5,47 +5,6 @@ import java.util.*;
 /* #2 Add Two Numbers
  * href: https://leetcode.com/problems/add-two-numbers/ */
 public class AddTwoNumbers {
-
-    public static class ListNode{
-        int val;
-        ListNode next;
-        
-        ListNode(int value){
-            this.val = value;
-            this.next = null;
-        }
-    }
-    
-    public static ListNode arrayToList(int[] array){
-        /* head with dummy first node */
-        ListNode head = new ListNode(0);
-        ListNode t = head;
-        
-        for(int i = 0; i<array.length; i++){
-            t.next = new ListNode( array[i] );
-            t = t.next;
-        }
-        
-        return head.next;
-    }
-    
-    public static int[] listToArray(ListNode list){
-        List<Integer> arrayList = new ArrayList<Integer>();
-        
-        while(list!=null){
-            arrayList.add( list.val );
-            list = list.next;
-        }
-        
-        int[] array = new int[arrayList.size()];
-        
-        for(int i=0; i<arrayList.size(); i++){
-            array[i] = arrayList.get(i);
-        }
-        
-        return array;
-    }
-    
     public static ListNode solve(ListNode l1, ListNode l2){
         ListNode head = new ListNode(0); /* Dummy head */
         ListNode prevNode = head;
@@ -71,7 +30,7 @@ public class AddTwoNumbers {
             sum = val1 + val2 + carry;
             carry = sum/10;
             
-            prevNode.next = new AddTwoNumbers.ListNode( sum%10 );
+            prevNode.next = new ListNode( sum%10 );
             prevNode = prevNode.next;
         }
         
@@ -82,10 +41,10 @@ public class AddTwoNumbers {
         int[] n1 = new int[]{0, 0, 2, 4, 3};
         int[] n2 = new int[]{5, 6, 4};
         
-        ListNode l1 = arrayToList(n1);
-        ListNode l2 = arrayToList(n2);
+        ListNode l1 = ListNode.arrayToList(n1);
+        ListNode l2 = ListNode.arrayToList(n2);
         
-        System.out.println( Arrays.toString( listToArray(solve(l1, l2)) ) );;
+        System.out.println( Arrays.toString( ListNode.toArray(solve(l1, l2)) ) );;
     }
 
 }
