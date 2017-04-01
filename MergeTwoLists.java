63061e7264e352afd3d2bcc99ff89ce1d6193f13
Hassan Nadeem <m.hassan.nadeem@outlook.com>
2017-04-01 21:37:59 +0500

    Add #21

--- MergeTwoLists.java
+++ MergeTwoLists.java
@@ -9,18 +9,44 @@ import java.util.Arrays;
  * href: https://leetcode.com/problems/merge-two-sorted-lists/ */
 public class MergeTwoLists {
     
-    private static void appendAtEnd(ListNode head, ListNode node){
-        
-    }
-    
     public static ListNode solve(ListNode l1, ListNode l2){
-        return null;
+        ListNode dummyHead = new ListNode(Integer.MIN_VALUE);
+        ListNode tail = dummyHead;
+        
+        while(l1!=null && l2!=null){
+            if(l1.val < l2.val){
+                tail.next = l1;
+                l1 = l1.next;
+            }else{
+                tail.next = l2;
+                l2 = l2.next;
+            }
+            tail = tail.next;
+        }
+        
+        tail.next = l1!=null?l1:l2;
+        
+        return dummyHead.next;
     }
 
     
     @Test
     public void test(){
+        /* Empty List */
+        test(new int[]{1,2,3}, new int[]{});
+        test(new int[]{}, new int[]{1,2,3});
+        test(new int[]{}, new int[]{});
+        
+        /* Single Element List */
+        test(new int[]{4}, new int[]{1,2,3});
+        test(new int[]{1,2,3}, new int[]{4});
+        test(new int[]{1}, new int[]{2});
+        test(new int[]{2}, new int[]{1});
+        
+        /* Normal */
         test(new int[]{1,4,5}, new int[]{2,3});
+        test(new int[]{1,2,3,5}, new int[]{4,6});
+        test(new int[]{1,1,1,1,1,2,2,2,2,4,4,4,4}, new int[]{1,2,3,4,5,6});
     }
     
     public void test(int[] l1, int[] l2){
