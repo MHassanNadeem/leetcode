e1d6ef234c3225d7c84087f616997dbaa60d1e1e
Hassan Nadeem <m.hassan.nadeem@outlook.com>
2017-06-07 20:00:48 +0500

    Add another solution for #75

--- SortColors.java
+++ SortColors.java
@@ -9,8 +9,28 @@ import java.util.Arrays;
  * href: https://leetcode.com/problems/sort-colors/#/description */
 public class SortColors {
     
-    /* O(n) accepted solution. */
+    /* O(n) accepted solution. Slightly faster than solve2() */
     public static void solve(int[] nums){
+        int index_0 = 0;
+        int index_2 = nums.length - 1;
+        
+        for(int i=0; i<=index_2 && i>=index_0; ){
+            if(nums[i] == 2){
+                /* swap elements i and index_2 */
+                nums[i] = nums[index_2];
+                nums[index_2--] = 2;
+            }else if(nums[i] == 0 && i>index_0){
+                /* swap elements i and index_0 */
+                nums[i] = nums[index_0];
+                nums[index_0++] = 0;
+            }else{
+                i++;
+            }
+        }
+    }
+    
+    /* O(n) accepted solution. */
+    public static void solve2(int[] nums){
         int[] count = new int[3];
         
         for(int n:nums) count[n]++;
@@ -24,6 +44,8 @@ public class SortColors {
     @Test
     public void test(){
         test(new int[]{0});
+        test(new int[]{1});
+        test(new int[]{2});
         test(new int[]{0, 1, 2});
         test(new int[]{2, 1, 0});
         test(new int[]{1, 2, 0});
